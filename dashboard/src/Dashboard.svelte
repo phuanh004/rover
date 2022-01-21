1b7d969f238e7a8c4b31c6ffdb4dfa547bb988bb
phuanh004 <phuanh004@gmail.com>
2022-01-21 13:38:25 -0800

    Remove distance sensor in dashboard

--- dashboard/src/Dashboard.svelte
+++ dashboard/src/Dashboard.svelte
@@ -1,64 +1,56 @@
 <script>
-    import LineGraph from "./modules/LineGraph.svelte";
+  import LineGraph from "./modules/LineGraph.svelte";
 
-    const HOST_NAME = PI_HOST_NAME;
-    const PORT = PI_PORT;
+  const HOST_NAME = PI_HOST_NAME;
+  const PORT = PI_PORT;
 </script>
 
 <main>
-    <div class="container mx-auto">
-        <div class='row'>
-            <div class='column'>
-                <LineGraph
-                    label="Temperature (°C)"
-                    api_obj_name="temperature_c"
-                    src="ws://{HOST_NAME}:{PORT}/temperature"
-                    color="rgb(247,85,144)"
-                />
-            </div>
-            <div class='column'>
-                <LineGraph
-                    label="Humidity"
-                    api_obj_name="humidity"
-                    src="ws://{HOST_NAME}:{PORT}/humidity"
-                    color="rgb(1,167,194)"
-                />
-            </div>
-        </div>
-        <div class="row">
-            <div class='column'>
-                <LineGraph
-                    label="Radiation"
-                    api_obj_name="radiation"
-                    src="ws://{HOST_NAME}:{PORT}/radiation"
-                    color="rgb(231,192,96)"
-                />
-            </div>
-            <div class='column'>
-                <LineGraph
-                    label="Distance"
-                    api_obj_name="distance"
-                    src="ws://{HOST_NAME}:{PORT}/distance"
-                    color="rgb(231,192,96)"
-                />
-            </div>
-        </div>
+  <div class="container mx-auto">
+    <div class="row">
+      <div class="column">
+        <LineGraph
+          label="Temperature (°C)"
+          api_obj_name="temperature_c"
+          src="ws://{HOST_NAME}:{PORT}/temperature"
+          color="rgb(247,85,144)"
+        />
+      </div>
+      <div class="column">
+        <LineGraph
+          label="Humidity"
+          api_obj_name="humidity"
+          src="ws://{HOST_NAME}:{PORT}/humidity"
+          color="rgb(1,167,194)"
+        />
+      </div>
     </div>
+    <div class="row">
+      <div class="column">
+        <LineGraph
+          label="Radiation"
+          api_obj_name="radiation"
+          src="ws://{HOST_NAME}:{PORT}/radiation"
+          color="rgb(231,192,96)"
+        />
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-    .row {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        width: 100%;
-        gap: 2em;
-        margin-bottom: 2em;
-    }
+  .row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    gap: 2em;
+    margin-bottom: 2em;
+  }
 
-    .column {
-        display: flex;
-        flex-direction: column;
-        flex: 1;
-    }
+  .column {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
 </style>
