b004fcfd8c32d56bbf6ccfea1ee403e9e4c7fdc6
phuanh004 <phuanh004@gmail.com>
2022-01-21 13:57:12 -0800

    Fix chart responsive problem

--- dashboard/src/Dashboard.svelte
+++ dashboard/src/Dashboard.svelte
@@ -6,51 +6,25 @@
 </script>
 
 <main>
-  <div class="container mx-auto">
-    <div class="row">
-      <div class="column">
-        <LineGraph
-          label="Temperature (°C)"
-          api_obj_name="temperature_c"
-          src="ws://{HOST_NAME}:{PORT}/temperature"
-          color="rgb(247,85,144)"
-        />
-      </div>
-      <div class="column">
-        <LineGraph
-          label="Humidity"
-          api_obj_name="humidity"
-          src="ws://{HOST_NAME}:{PORT}/humidity"
-          color="rgb(1,167,194)"
-        />
-      </div>
-    </div>
-    <div class="row">
-      <div class="column">
-        <LineGraph
-          label="Radiation"
-          api_obj_name="radiation"
-          src="ws://{HOST_NAME}:{PORT}/radiation"
-          color="rgb(231,192,96)"
-        />
-      </div>
-    </div>
+  <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+    <LineGraph
+      label="Temperature (°C)"
+      api_obj_name="temperature_c"
+      src="ws://{HOST_NAME}:{PORT}/temperature"
+      color="rgb(247,85,144)"
+    />
+    <LineGraph
+      label="Humidity"
+      api_obj_name="humidity"
+      src="ws://{HOST_NAME}:{PORT}/humidity"
+      color="rgb(1,167,194)"
+    />
+
+    <LineGraph
+      label="Radiation"
+      api_obj_name="radiation"
+      src="ws://{HOST_NAME}:{PORT}/radiation"
+      color="rgb(231,192,96)"
+    />
   </div>
 </main>
-
-<style>
-  .row {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100%;
-    gap: 2em;
-    margin-bottom: 2em;
-  }
-
-  .column {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-  }
-</style>
